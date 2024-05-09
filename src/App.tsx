180c7bd11753dfbf76081e7d7b4ef36850b0ee0e
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2024-05-09 15:46:21 +0200

    Add comments to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,32 @@ function App() {
   const modalTabIndex = 1;
   const printingSectionTabIndex = 2;
 
+  // State to create a class for notify pop up
   const [notifyClass] = useState(nanoid());
+
+  // State for printing, paper format and image configuration
   const [paperConfig, setPaperConfig] = useState(paperUtils.DefaultSizes.a4 as IPaperConfig);
   const [imageConfig, setImageConfig] = useState(null as null | IImageConfig);
   const [isPrintingViewEnabled, setIsPrintingViewEnabled] = useState(false);
+
+  // State for handling modal
   const [modalContent, setModalContent] = useState(null as ReactNode);
   const [isModalEnabled, setIsModalEnabled] = useState(false);
+
+  // Background color is switched when printing view is enabled/disabled
   const [isBackgroundColored, setIsBackgroundColored] = useState(true);
 
   function disablePrintingView() {
+    // Find and remove the notify pop up if exists
     const notify = document.querySelector(`.a${notifyClass}`);
     notify?.parentElement?.removeChild(notify);
+
+    // Restore background color and disable printing view
     setIsBackgroundColored(true);
     setIsPrintingViewEnabled(false);
   }
 
+  // Update component when background color is changed
   useEffect(() => {
     const bodyElem = document.querySelector("body");
     if (bodyElem)
@@ -40,15 +51,19 @@ function App() {
   return (
     <>
       <header className={css.Header}>
-        <h1 className={css.Title} hidden={isPrintingViewEnabled}>
+        <h1
+          className={css.Title}
+          /* Show/hide if printing view is enabled */
+          hidden={isPrintingViewEnabled}>
           PolTailorEx
         </h1>
       </header>
       <main>
         <section
+          className={css.Section}
+          /* Show/hide if printing view is enabled */
           hidden={isPrintingViewEnabled}
-          style={{ display: isPrintingViewEnabled ? "none" : "flex" }}
-          className={css.Section}>
+          style={{ display: isPrintingViewEnabled ? "none" : "flex" }}>
           <PatternSetter setImageConfig={setImageConfig} setModalChildren={setModalContent} />
           <FormatSetter
             paperConfig={paperConfig}
@@ -57,7 +72,9 @@ function App() {
             setPaperConfig={setPaperConfig}
           />
           <button
+            /* Disable the button if there is no imageConfig ready */
             disabled={!imageConfig}
+            /* Show notification that the printing view can be exited with an ESC key and enable printing view */
             onClick={() => {
               Notify.info("Press ESC to exit printing view.", {
                 timeout: 5000,
@@ -71,16 +88,21 @@ function App() {
         </section>
       </main>
       <section
+        /* Focus on the section when loaded so it can be exited with ESC key */
+        tabIndex={printingSectionTabIndex}
         onLoad={(e) => {
           e.currentTarget.focus();
         }}
-        tabIndex={printingSectionTabIndex}
+        /* Show/hide the printing view */
+        style={{ display: isPrintingViewEnabled ? "block" : "none" }}
         hidden={!isPrintingViewEnabled}
+        /* Close the printing view when ESC key is pressed */
         onKeyDown={(e) => {
           if (e.key === "Escape") {
             disablePrintingView();
           }
         }}>
+        {/* Show the printing view and printing view exit button when imageConfig is ready and printing view is enabled */}
         {imageConfig && isPrintingViewEnabled && (
           <>
             <button className={css.BackButton} onClick={disablePrintingView}>
