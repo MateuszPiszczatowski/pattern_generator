a23aba89149b5bde4ca8edc8b4b52f89a3af240b
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2024-05-18 18:59:11 +0200

    Add comments to sizesHelper

--- src/Components/SizesHelper/SizesHelper.tsx
+++ src/Components/SizesHelper/SizesHelper.tsx
@@ -2,9 +2,13 @@ import { IImageConfig, IPaperConfig } from "../../utils/interfaces-n-types";
 import { PaperConfig } from "../../utils/paperUtils";
 import PrintSplitter from "../PrintSplitter/PrintSplitter";
 
+// A component that draws a square that can be used to check if the pattern will be printed correctly
 export default function SizesHelper({ paperConfig }: ISizesHelperProps) {
+  // Set sizesFactor in the same way as in patterns
   const sizesFactor = 100;
+  // Create SVG element
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  // Set SVG element sizes and attributes
   const width =
     (paperConfig.width - paperConfig.margin.left - paperConfig.margin.right) * sizesFactor;
   const height =
@@ -18,12 +22,14 @@ export default function SizesHelper({ paperConfig }: ISizesHelperProps) {
     paperConfig.unit === "mm" ? `${sizesFactor}` : `${0.1 * sizesFactor}`
   );
   let size = paperConfig.unit === "mm" ? 10 * sizesFactor : sizesFactor;
+  // Try to fit the square in the avalible space, max width is 5x5
   for (let i = 5; i > 1; i--) {
     if (size * i < width && size * i < height) {
       size = size * i;
       break;
     }
   }
+  // Create a text svg element that displayes what are the sizes
   const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
   text.textContent = `${size / 100}x${size / 100}${paperConfig.unit}`;
   text.setAttribute("font-size", `${size / 5}`);
@@ -32,20 +38,24 @@ export default function SizesHelper({ paperConfig }: ISizesHelperProps) {
   text.setAttribute("fill", "black");
   text.setAttribute("stroke", "none");
   svg.appendChild(text);
+  // Create the svg rectangle
   const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
   rect.setAttribute("x", `${width / 2 - size / 2}`);
   rect.setAttribute("y", `${height / 2 - size / 2}`);
   rect.setAttribute("width", `${size}`);
   rect.setAttribute("height", `${size}`);
   svg.appendChild(rect);
+  // Create dataurl from the SVG element
   const svgString = new XMLSerializer().serializeToString(svg);
   const dataUrl = "data:image/svg+xml," + encodeURIComponent(svgString);
+  // Create imageConfig from the data
   const image: IImageConfig = {
     width: width / sizesFactor,
     height: height / sizesFactor,
     unit: paperConfig.unit,
     source: dataUrl,
   };
+  // Create the appropiate paper config
   const sizesPaperConfig = new PaperConfig(
     paperConfig.unit,
     width / sizesFactor,
@@ -55,6 +65,7 @@ export default function SizesHelper({ paperConfig }: ISizesHelperProps) {
     false,
     { top: 0, right: 0, bottom: 0, left: 0 }
   );
+  // Return the image in the print splitter to ensure, that it is shown in the same way as the pattern would
   return (
     <div
       style={{
